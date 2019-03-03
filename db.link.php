e4087c033587fb7d50fa581699a45d8db91cfac9
Gilson Meira <gilsonzi@yahoo.com>
2019-03-03 13:56:55 -0300

    Create db.link.php

---
+++ db.link.php
@@ -0,0 +1,33 @@
+<?php
+	class db{
+		//Host
+		private $host = 'localhost';
+
+		//Usuario
+		private $usuario = 'root';
+
+		//Senha
+		private $password = '';
+
+		//Banco de Dados
+		private $database = 'visibil_tecnologia';
+
+		public function conecta_mysql(){
+
+			//Cria uma conexão
+			$con = mysqli_connect($this->host, $this->usuario, $this->password, $this->database);
+
+			//Seta o banco de dados para UTF-8
+			mysqli_set_charset($con, 'utf-8');
+
+			//Verifica se existe erro na conexão
+			if(mysqli_connect_errno()){
+				echo "Erro ao conectar com o BD MySQL:".mysqli_connect_errno();
+			}
+
+			//Retorna a conexão efetuada		
+			return $con;
+		}
+	}
+
+?>
